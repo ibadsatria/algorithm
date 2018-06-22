d5a9b2e5ded2563e1fa445b6ce98a3e67d11f193
salman <ibadurrahman.salman@gmail.com>
2018-06-23 01:11:33 +0700

    Change Data Structure
    data structure of graph previously is map[vertex][]*vertex. Now it's map[*vertex][]*vertex

--- graph/bfs/graph.go
+++ graph/bfs/graph.go
@@ -2,39 +2,40 @@ package bfs
 
 // Graph domain
 type graph struct {
-	vertexEdge map[Vertex][]*Vertex
+	vertexEdge map[*Vertex][]*Vertex
 }
 
 // Traverse graph bfs manner
+// BFS algorithm
 func (g *graph) Traverse(s *Vertex) (result []int) {
+	var queue []*Vertex
 	s.Visited = true
-	result = append(result, s.ID)
+	queue = append(queue, s)
 
-	for key, value := range g.vertexEdge {
-		if !key.Visited {
-			key.Visited = true
-
-			for _, v := range value {
-				if !v.Visited {
-					v.Visited = true
-					result = append(result, v.ID)
-				}
-
+	for len(queue) != 0 {
+		temp := queue[:1][0]
+		queue = queue[1:]
+		result = append(result, temp.ID)
+		for _, v := range g.vertexEdge[temp] {
+			if !v.Visited {
+				v.Visited = true
+				queue = append(queue, v)
 			}
 		}
 	}
+
 	return
 }
 
 // AddEdge add connection between two vertex
-func (g *graph) AddEdge(verDomain, verRange Vertex) {
+func (g *graph) AddEdge(verDomain, verRange *Vertex) {
 	if g.vertexEdge == nil {
-		g.vertexEdge = make(map[Vertex][]*Vertex, 0)
+		g.vertexEdge = make(map[*Vertex][]*Vertex, 0)
 	}
 
 	if _, ok := g.vertexEdge[verDomain]; !ok {
-		g.vertexEdge[verDomain] = []*Vertex{&verRange}
+		g.vertexEdge[verDomain] = []*Vertex{verRange}
 	} else {
-		g.vertexEdge[verDomain] = append(g.vertexEdge[verDomain], &verRange)
+		g.vertexEdge[verDomain] = append(g.vertexEdge[verDomain], verRange)
 	}
 }
--- graph/bfs/graph_test.go
+++ graph/bfs/graph_test.go
@@ -1,6 +1,7 @@
 package bfs
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -16,25 +17,46 @@ func TestAddEdge(t *testing.T) {
 	}
 
 	t.Run("TestAddEdge", func(t *testing.T) {
-		g.AddEdge(listVer[0], listVer[1])
-		g.AddEdge(listVer[0], listVer[2])
-		g.AddEdge(listVer[1], listVer[2])
-		g.AddEdge(listVer[2], listVer[0])
-		g.AddEdge(listVer[2], listVer[3])
-		g.AddEdge(listVer[3], listVer[3])
+		g.AddEdge(&listVer[0], &listVer[1])
+		g.AddEdge(&listVer[0], &listVer[2])
+		g.AddEdge(&listVer[1], &listVer[2])
+		g.AddEdge(&listVer[2], &listVer[0])
+		g.AddEdge(&listVer[2], &listVer[3])
+		g.AddEdge(&listVer[3], &listVer[3])
 
 		//for vertex 0
-		assert.Contains(t, g.vertexEdge[listVer[0]], &listVer[1])
-		assert.Contains(t, g.vertexEdge[listVer[0]], &listVer[2])
+		assert.Contains(t, g.vertexEdge[&listVer[0]], &listVer[1])
+		assert.Contains(t, g.vertexEdge[&listVer[0]], &listVer[2])
 
 		// for Vertex 1
-		assert.Contains(t, g.vertexEdge[listVer[1]], &listVer[2])
+		assert.Contains(t, g.vertexEdge[&listVer[1]], &listVer[2])
 
 		// for vertex 2
-		assert.Contains(t, g.vertexEdge[listVer[2]], &listVer[0])
-		assert.Contains(t, g.vertexEdge[listVer[2]], &listVer[3])
+		assert.Contains(t, g.vertexEdge[&listVer[2]], &listVer[0])
+		assert.Contains(t, g.vertexEdge[&listVer[2]], &listVer[3])
 
 		// for vertex 3
-		assert.Contains(t, g.vertexEdge[listVer[3]], &listVer[3])
+		assert.Contains(t, g.vertexEdge[&listVer[3]], &listVer[3])
 	})
+}
+
+func ExampleTraverse() {
+	var g graph
+	listVer := []Vertex{
+		Vertex{0, false},
+		Vertex{1, false},
+		Vertex{2, false},
+		Vertex{3, false},
+	}
+
+	g.AddEdge(&listVer[0], &listVer[1])
+	g.AddEdge(&listVer[0], &listVer[2])
+	g.AddEdge(&listVer[1], &listVer[2])
+	g.AddEdge(&listVer[2], &listVer[0])
+	g.AddEdge(&listVer[2], &listVer[3])
+	g.AddEdge(&listVer[3], &listVer[3])
+
+	result := g.Traverse(&listVer[2])
+	fmt.Println(result)
+	// Output: [2 0 3 1]
 }
